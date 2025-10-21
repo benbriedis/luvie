7c83fb8ad29cc20191f8345ef0cb0bf7cdfaa18c
Ben Briedis <benbriedis@gmail.com>
2025-10-21 19:43:10 +1100

    Removing unused stuff

--- plugins/eg-fifths.lv2/fifths.c
+++ plugins/eg-fifths.lv2/fifths.c
@@ -1,3 +1,6 @@
+// Copyright 2014-2016 David Robillard <d@drobilla.net>
+// SPDX-License-Identifier: ISC
+
 #include "uris.h"
 
 #include <lv2/atom/atom.h>
@@ -32,12 +35,10 @@ typedef struct {
 static void
 connect_port(LV2_Handle instance, uint32_t port, void* data)
 {
-printf("In fifths connect_port()\n");	
   Fifths* self = (Fifths*)instance;
   switch (port) {
   case FIFTHS_IN:
     self->in_port = (const LV2_Atom_Sequence*)data;
-	//self->control_port_in = (LV2_Atom_Sequence*)data;
     break;
   case FIFTHS_OUT:
     self->out_port = (LV2_Atom_Sequence*)data;
@@ -90,7 +91,7 @@ static void
 run(LV2_Handle instance, uint32_t sample_count)
 {
   Fifths*           self = (Fifths*)instance;
-//  const FifthsURIs* uris = &self->uris;
+  const FifthsURIs* uris = &self->uris;
 
   // Struct for a 3 byte MIDI event, used for writing notes
   typedef struct {
@@ -98,27 +99,59 @@ run(LV2_Handle instance, uint32_t sample_count)
     uint8_t        msg[3];
   } MIDINoteEvent;
 
+  // Initially self->out_port contains a Chunk with size set to capacity
 
-  	/* Initially self->out_port contains a Chunk with size set to capacity */
-	const uint32_t out_capacity = self->out_port->atom.size;
+  // Get the capacity
+  const uint32_t out_capacity = self->out_port->atom.size;
 
-	/* Write an empty Sequence header to the output */
-	lv2_atom_sequence_clear(self->out_port);
-	self->out_port->atom.type = self->uris.atom_Sequence;
+  // Write an empty Sequence header to the output
+  lv2_atom_sequence_clear(self->out_port);
+printf("atom.type: %d\n",self->in_port->atom.type);	
+  self->out_port->atom.type = self->in_port->atom.type;
 
-			MIDINoteEvent note;
-//			note.event.time.frames = 0;
-			note.event.time.frames = 100;
-			note.event.body.type =  self->uris.midi_Event;
-			note.event.body.size = 3;
-			note.msg[0] = 0x80; 	// Note off
-			note.msg[1] = 60;
-			note.msg[2] = 0;
-			lv2_atom_sequence_append_event(self->out_port, out_capacity, &note.event);
+  // Read incoming events
+  LV2_ATOM_SEQUENCE_FOREACH (self->in_port, ev) {
+    if (ev->body.type == uris->midi_Event) {
+      const uint8_t* const msg = (const uint8_t*)(ev + 1);
 
+      switch (lv2_midi_message_type(msg)) {
+      case LV2_MIDI_MSG_NOTE_ON:
+      case LV2_MIDI_MSG_NOTE_OFF:
+        // Forward note to output
+        lv2_atom_sequence_append_event(self->out_port, out_capacity, ev);
 
-printf("run()  SENT A NOTE\n");	
+printf("frames: %ld\n",ev->time.frames);
+printf("type: %d\n",ev->body.type);
+printf("size: %d\n",ev->body.size);
+printf("msg[0]: %d\n",msg[0]);
+printf("msg[1]: %d\n",msg[1]);
+printf("msg[2]: %d\n",msg[2]);
 
+        if (msg[1] <= 127 - 7) {
+          // Make a note one 5th (7 semitones) higher than input
+          MIDINoteEvent fifth;
+
+          // Could simply do fifth.event = *ev here instead...
+          fifth.event.time.frames = ev->time.frames; // Same time
+          fifth.event.body.type   = ev->body.type;   // Same type
+          fifth.event.body.size   = ev->body.size;   // Same size
+
+          fifth.msg[0] = msg[0];     // Same status
+          fifth.msg[1] = msg[1] + 7; // Pitch up 7 semitones
+          fifth.msg[2] = msg[2];     // Same velocity
+
+          // Write 5th event
+          lv2_atom_sequence_append_event(
+            self->out_port, out_capacity, &fifth.event);
+        }
+        break;
+      default:
+        // Forward all other MIDI events directly
+        lv2_atom_sequence_append_event(self->out_port, out_capacity, ev);
+        break;
+      }
+    }
+  }
 }
 
 static const void*
@@ -141,4 +174,3 @@ lv2_descriptor(uint32_t index)
 {
   return index == 0 ? &descriptor : NULL;
 }
-
