9f7f79d4bf042906a3b8cb374ff5478b2c9c8add
Ben Briedis <benbriedis@gmail.com>
2026-02-01 11:27:40 +1100

    Separated gridCell()

--- testiced/src/gridArea/grid.rs
+++ testiced/src/gridArea/grid.rs
@@ -73,26 +73,6 @@ impl<'a> Grid<'a> {
             mode: CursorMode::INIT,
         }
     }
-
-    fn drawCell(&self,frame:&mut Frame<Renderer>,origin:Point, c: Cell)
-    {
-        let point = |x:f32,y:f32| Point {x: origin.x + x, y: origin.y + y};
-//        let point = |x:f32,y:f32| Point {x, y};
-
-        let s = self.settings;
-        let lineWidth = 1.0;
-
-         //TODO possibly size to be inside grid, although maybe not at start
-        let point = point(c.col as f32 * s.colWidth, c.row as f32 * s.rowHeight +lineWidth);
-        let size = Size::new(c.length * s.colWidth, s.rowHeight - 2.0 * lineWidth);
-        let path = Path::rectangle(point,size);
-        frame.fill(&path, Color::from_rgb8(0x12, 0x93, 0xD8));
-
-        /* Add the dark line on the left: */
-        let size2 = Size::new(8.0, s.rowHeight - 2.0 * lineWidth);
-        let path2 = Path::rectangle(point,size2);
-        frame.fill(&path2, Color::from_rgb8(0x12, 0x60, 0x90));
-    }
 }
 
 
@@ -210,15 +190,15 @@ println!("origin: {:?}",origin);
                 match self.mode {
                     CursorMode::MOVING(ref data) => {
                         if i != data.cellIndex {
-                            self.drawCell(frame,origin,*c);
+                            drawCell(self,frame,origin,*c);
                         }
                     }
                     CursorMode::RESIZING(ref data) => {
                         if i != data.cellIndex {
-                            self.drawCell(frame,origin,*c);
+                            drawCell(self,frame,origin,*c);
                         }
                     }
-                    _ => self.drawCell(frame,origin,*c)
+                    _ => drawCell(self,frame,origin,*c)
 //FIXME remove
 /*                    
                     _ => {
@@ -233,8 +213,8 @@ println!("origin: {:?}",origin);
 
             /* Draw selected note last so it sits on top */
             match self.mode {
-                CursorMode::MOVING(ref data) => self.drawCell(frame,origin,data.workingCell),
-                CursorMode::RESIZING(ref data) => self.drawCell(frame,origin,data.workingCell),
+                CursorMode::MOVING(ref data) => drawCell(self,frame,origin,data.workingCell),
+                CursorMode::RESIZING(ref data) => drawCell(self,frame,origin,data.workingCell),
                 _ => ()
             }
 
@@ -411,3 +391,104 @@ impl<'a> From<Grid<'a>> for Element<'a,GridAreaMessage> {
     }
 }
 
+
+fn drawCell(grid: &Grid,frame:&mut Frame<Renderer>,origin:Point, c: Cell)
+{
+    let point = |x:f32,y:f32| Point {x: origin.x + x, y: origin.y + y};
+//        let point = |x:f32,y:f32| Point {x, y};
+
+    let s = grid.settings;
+    let lineWidth = 1.0;
+
+     //TODO possibly size to be inside grid, although maybe not at start
+    let point = point(c.col as f32 * s.colWidth, c.row as f32 * s.rowHeight +lineWidth);
+    let size = Size::new(c.length * s.colWidth, s.rowHeight - 2.0 * lineWidth);
+    let path = Path::rectangle(point,size);
+    frame.fill(&path, Color::from_rgb8(0x12, 0x93, 0xD8));
+
+    /* Add the dark line on the left: */
+    let size2 = Size::new(8.0, s.rowHeight - 2.0 * lineWidth);
+    let path2 = Path::rectangle(point,size2);
+    frame.fill(&path2, Color::from_rgb8(0x12, 0x60, 0x90));
+}
+
+fn drawGrid(grid: &Grid,frame:&mut Frame<Renderer>, origin:Point)
+{
+    let point = |x:f32,y:f32| Point {x: origin.x + x, y: origin.y + y};
+
+    let s = grid.settings;
+
+    //XXX in theory only need to draw the horizontal lines once so long as the cells sit inside them 
+    //XXX could possibly omit redrawing cells not on the last row too.
+
+    /* Draw the grid horizontal lines: */
+    for i in 0..=s.numRows {
+        let line = Path::line(
+            point(0.0, i as f32 * s.rowHeight),
+            point(s.numCols as f32 * s.colWidth, i as f32 * s.rowHeight)
+        );
+
+        frame.stroke(&line, Stroke {
+            width: 1.0,
+            style: stroke::Style::Solid(Color::from_rgb8(0x12, 0xee, 0x12)),
+//                    style: stroke::Style::Solid(exPalette.primary.base.color),
+            ..Stroke::default()
+        });
+    }
+
+    /* Draw the grid vertical lines: */
+    for i in 0..=s.numCols {
+        let line = Path::line(
+            point(i as f32 * s.colWidth, 0.0),
+            point(i as f32 * s.colWidth, s.numRows as f32 * s.rowHeight)
+        );
+
+        frame.stroke(&line, Stroke {
+            width: 1.0,
+            style: stroke::Style::Solid(Color::from_rgb8(0xee, 0x12, 0x12)),
+//                    style: stroke::Style::Solid(exPalette.secondary.base.color),
+            ..Stroke::default()
+        });
+    }
+
+    /* Draw cells: */
+    for (i,c) in grid.cells.iter().enumerate() {
+        match grid.mode {
+            CursorMode::MOVING(ref data) => {
+                if i != data.cellIndex {
+                    drawCell(grid,frame,origin,*c);
+                }
+            }
+            CursorMode::RESIZING(ref data) => {
+                if i != data.cellIndex {
+                    drawCell(grid,frame,origin,*c);
+                }
+            }
+            _ => drawCell(grid,frame,origin,*c)
+//FIXME remove
+/*                    
+            _ => {
+                let pnt = point(c.col as f32 * s.colWidth, c.row as f32 * s.rowHeight +1.0);
+                let size = Size::new(c.length * s.colWidth, s.rowHeight - 2.0 * 1.0);
+                let path = Path::rectangle(pnt,size);
+                frame.fill(&path, Color::from_rgb8(0x12, 0x93, 0xD8));
+            }
+*/
+        }
+    };
+
+    /* Draw selected note last so it sits on top */
+    match grid.mode {
+        CursorMode::MOVING(ref data) => drawCell(grid,frame,origin,data.workingCell),
+        CursorMode::RESIZING(ref data) => drawCell(grid,frame,origin,data.workingCell),
+        _ => ()
+    }
+
+    /* Draw the app border: */
+/*            
+    frame.stroke(
+        &canvas::Path::rectangle(Point::ORIGIN, frame.size()),
+        canvas::Stroke::default(),
+    );
+*/
+}
